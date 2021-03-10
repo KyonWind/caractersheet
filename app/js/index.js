82a0a5cd3206b51cf02f5baf3174baef54618330
MZZXL <victor.cequeamijares.ext@bayer.com>
2021-03-10 12:12:18 -0300

    stats

--- app/js/index.js
+++ app/js/index.js
@@ -10,9 +10,21 @@ window.onload = function(e){
         {STA: "WIS", Des: "WISDOM"},
         {STA: "CAR", Des: "CHARISMA"}
     ];
+
+    const StatsModifier = 
+    [
+        {VALUE: "", Modifier: ""},
+        {VALUE: "", Modifier: ""},
+        {VALUE: "", Modifier: ""},
+        {VALUE: "", Modifier: ""},
+        {VALUE: "", Modifier: ""},
+        {VALUE: "", Modifier: ""}
+    ];
     
     headerMap(headerNames);
     statsMap(abName);
+
+
 };
 
 const headerMap = (data) =>  {
@@ -34,15 +46,61 @@ const headerMap = (data) =>  {
 
 const statsMap = (data) =>{
     const StaNamelist = document.querySelector(".StaNamelist")
+    const StatValuelist = document.querySelector(".StatValuelist")
+    const StatvModifierlist = document.querySelector(".StatvModifierlist")
+    const StatTempValue = document.querySelector(".StatTempValue")
+    const StatTempModifier = document.querySelector(".StatTempModifier")
     let query = ``;
+    
     data.forEach(element => {
         query += `<li class=" flex flex-jc-c">
         <h1>${element.STA}</h1>
         <p>${element.Des}</p>
     </li>`
     });
-    StaNamelist.innerHTML += query;
-};
 
+    StaNamelist.innerHTML += query;
+    query = ``;
+
+    data.forEach(element => {
+        query += `
+        <li class="flex flex-jc-c">
+        <input type="number" value="0" class="${element.STA}-Value">
+        </li>`
+    });
+    
+    StatValuelist.innerHTML += query;
+    query = ``;
+
+    data.forEach(element => {
+        query += `
+        <li class=" flex flex-jc-c">
+        <p class="${element.STA}-Modifier">30</p>
+        </li>`
+    });
+
+    StatvModifierlist.innerHTML += query;
+    query = ``;
+
+    data.forEach(element => {
+        query += `
+        <li class="flex flex-jc-c">
+        <input type="number" value="0" class="${element.STA}-TValue">
+        </li>`
+    });
+
+    StatTempValue.innerHTML += query;
+    query = ``;
+
+    data.forEach(element => {
+        query += `
+        <li class=" flex flex-jc-c">
+        <p class="${element.STA}-TModifier">30</p>
+        </li>`
+    });
+
+    StatTempModifier.innerHTML += query;
+    query = ``;
+};
 
    
